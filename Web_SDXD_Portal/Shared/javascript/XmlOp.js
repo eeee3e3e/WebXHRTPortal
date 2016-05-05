484e69ff87fc7c70b53a3fa992b53d8d964d1e57
eeee3e3e <yy_double_y@126.com>
2016-05-05 13:55:46 +0800

    0505

--- Web_SDXD_Portal/Shared/javascript/XmlOp.js
+++ Web_SDXD_Portal/Shared/javascript/XmlOp.js
@@ -1,52 +1,193 @@
-﻿//getPath - get file path
-//linkID - key tag
-//xmlBranchObj - branch node
-function getPath(linkTag,linkID,xmlBranchObj)
+﻿var strPath="";// page variable of xml path;
+var strStatus="0"; //search status  0 - result not found |  1 - result found
+var returnValue=new Array(2);
+returnValue[0]=strPath;
+returnValue[1]=strStatus;
+//Create path as name1-name2-name3-name4
+//objNode - currentNode (if there is a currentNode , then find its parentNode and create the full path until root node)
+//strPath - the file path
+//tagName - current node tag name
+//
+function PathMaker(objNode,strPath,tagName)
 {
-    // var returnValue=null;
-    for(var iNum=0;iNum=xmlBranchObj.length;iNum++)
+    var strPath="";
+    var strPathArray=new Array(); 
+    //&&RootNode.childNodes[0].nodeType!=3
+    while(objNode.parentNode.nodeName!="Root")
     {
-        var xmlChildNode=xmlBranchObj[iNum].childNodes;
-        if(xmlChildNode.nodename==linkTag)
+        strPathArray.push(objNode.parentNode.nodeName);
+        objNode=objNode.parentNode;
+    }
+	
+    for(var iCount=strPathArray.length-1; iCount>0;iCount--)
+    {
+        strPath+=strPathArray[iCount];
+        strPath+="-";
+    }
+	
+	
+    return strPath+tagName;
+}
+
+function getChildNodes(ObjChildNode)
+{
+    return ObjChildNode.childNodes;
+}
+
+function getPath(linkID,xmlFilePath,RootNode,strField)
+{
+    //var strPath="";
+    if (RootNode == "Root")
+    {
+        strPath = RootNode;
+        //load xml files
+        var xmlObj = loadXMLDoc(xmlFilePath);
+        //get child node from current node
+        if(RootNode==null)
         {
-            //traverse the child node
-            for()
-
+            return false;
         }
-    }
-}
+        else
+        {
+            try 
+            {
+                var childNodes = getChildNodes(xmlObj.getElementsByTagName(RootNode)[0]);
+            }
+            catch (err) 
+            {
+                alert(err.message);  
+            }
+            //typeof(reValue) == "undefined"
+			
+            //traverse child nodes;
+			
+		
+            for (var iNum = 0; iNum < childNodes.length; iNum++) 
+            {
+                if (childNodes[iNum].nodeName == strField && childNodes[iNum].nodeTypedValue == linkID) 
+                {
+                    strPath=PathMaker(childNodes[iNum],strPath, childNodes[iNum].nodeTypedValue);
+                    returnValue[0] = strPath; //{strPath,"1"};
+                    returnValue[1] = "1";
+                    break;
+                }
+                else
+                {
+                    if(returnValue[1]=="1")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        returnValue = getPath(linkID, xmlFilePath, childNodes[iNum], strField);
+                    }
+                }
+            }
+        }
 
-//get web path according to the ID of xml file
-//add by YangDu 0429
-function getPath(linkID,xmlFilePath)
-{
-    var xml = loadXMLDoc1(xmlFilePath);
-    var object = xml.getElementByTagName(linkID);
-    for(var iNum=0;iNum<object.length;iNum++)
+        ////search for specific products/services and return objects&nodes (an array)
+        //var objects = xml.getElementByTagName(linkID);
+        ////search node which contain the key value (linkID)
+        //for (var iNum = 0; iNum < object.length; iNum++) //
+        //{
+        //    if (object[iNum].getElementByTagName(""));
+        //}
+    }
+    else
     {
-        if(object[iNum].getElementByTagName(""))
+        //typeof(childNodes)=="undefined")
+        //alert(RootNode.childNodes[0].nodeType);
+        if(RootNode.hasChildNodes()&&RootNode.childNodes[0].nodeType!=3)  // if current node is not the leaf node
+        {
+            strPath = RootNode.nodeName;
+            try
+            {
+                var childNodes = getChildNodes(RootNode);
+            }
+            catch (err)
+            {
+                alert(err.message);
+                return false; //deal with exception.
+            }
+
+		
+		
+            for (var iNum = 0; iNum < childNodes.length; iNum++) 
+            {
+                if (childNodes[iNum].nodeName == strField && childNodes[iNum].nodeTypedValue == linkID) 	
+                {
+                    strPath = PathMaker(childNodes[iNum],strPath, childNodes[iNum].nodeTypedValue);
+                    returnValue[0]=strPath;//{strPath,"1"};
+                    returnValue[1]="1";
+                    break;
+                }	
+                else 
+                {
+                    if(returnValue[1]=="1")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        returnValue=getPath(linkID, xmlFilePath, childNodes[iNum], strField);
+                    }
+                }
+            }
+        }
+        else  // if the node is the final leaf node
+        {
+            returnValue[0]=strPath;//{strPath,"0"};
+            returnValue[1]="0";
+            return returnValue;
+        }
+		
     }
+    return returnValue;
 }
 
-//get img Url from xml file.
-function getUrl(linkID)
-{
+////get img Url from xml file.
+//function getUrl(linkID)
+//{
 
+//}
+
+////there are two kinds of methods of loading xml
+////1,using xpath
+////2,using javascript to retrival
+////loading xml file
+function loadXMLDoc(xml_name) 
+{ 
+    var xmlDoc; 
+    try 
+    { 
+        xmlDoc = new ActiveXObject("Microsoft.XMLDOM"); // Support IE 
+    } 
+    catch(e) 
+    { 
+        try 
+        { 
+            // Support Firefox, Mozilla, Opera, etc 
+            xmlDoc = document.implementation.createDocument("", "", null) ;// 创建一个空的 XML 文档对象。 
+        } 
+        catch(e) 
+        { 
+            alert(e.message); 
+        } 
+    } 
+    // 加载XML文档 
+    try 
+    { 
+        xmlDoc.async = false; // 关闭异步加载 
+        xmlDoc.load(xml_name); 
+    } 
+    catch(e) 
+    { 
+        // alert(e.message) 如果浏览器是Chrome，则会catch这个异常：Object # (a Document) has no method "load"，所以，以下实现支持chrome加载XML文档（只是粗略的写下） 
+        var xhr = new XMLHttpRequest(); 
+        xhr.open("GET", xml_name, false); 
+        xhr.send(null); 
+        xmlDoc = xhr.responseXML.documentElement; 
+    } 
+    return xmlDoc; 
+	 
 }
-
-//there are two kinds of methods of loading xml
-//1,using xpath
-//2,using javascript to retrival
-//loading xml file
-function loadXMLDoc1(dname) {
-    if (window.XMLHttpRequest) {
-        xhttp = new XMLHttpRequest();
-    } else {
-        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xhttp.open("GET", dname, false);
-    xhttp.send("");
-    return xhttp.responseXML;
-}
-
-
